40962f3d9ed2d0de6ed86ced804a2d1fce00cef7
imamrendra <amrendra10017@gmail.com>
2026-01-25 18:37:51 +0530

    database connection

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async()=>{
+    try {
+        const connectInstance=await mongoose.connect
+        (`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`\n Mongodb connected !! DB HOST: 
+        ${connectInstance.connection.host}`);
+    } catch (error) {
+        console.error("Connection error:",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+dotenv.config();
+import connectDB from "./db/index.js";
+
+
+connectDB();
+
+
+
+
+
+
+
+
+/*
+import express from express;
+const app =express();
+
+;( async()=>{
+    try {
+        await mongoose.connect(`${peocess.env.
+        MONGODB_UR}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Err::",error);
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port $
+            {process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("Error Occurred", error);
+    }
+})();
+
+*/
